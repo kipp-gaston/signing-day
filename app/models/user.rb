3c89a1de36b45a874d4949f2e3ec917dc9b13b3d
Lawson Kurtz <lawson.kurtz@viget.com>
2015-04-25 01:06:12 -0600

    Remove user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,6 @@
 class User < ActiveRecord::Base
-  enum role: [:user, :vip, :admin]
-  after_initialize :set_default_role, :if => :new_record?
-
-  def set_default_role
-    self.role ||= :user
-  end
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end
